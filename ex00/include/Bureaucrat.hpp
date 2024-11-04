81fb1229ca575e948df77afdd5e91ce4e46ce35b
MalwarePup <71011961+MalwarePup@users.noreply.github.com>
2024-11-04 15:36:52 +0100

    style(Bureaucrat.hpp): reorder access specifiers for better readability and consistency

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -8,30 +8,30 @@
 
 class Bureaucrat
 {
-	private:
-		const std::string _name;
-		unsigned int _grade;
-	public:
-		Bureaucrat() {};
-		Bureaucrat(const std::string &name, unsigned int grade);
-		Bureaucrat(const Bureaucrat &b);
-		Bureaucrat &operator=(const Bureaucrat &b);
-		~Bureaucrat() {};
+private:
+	const std::string _name;
+	unsigned int _grade;
+public:
+	Bureaucrat() {};
+	Bureaucrat(const std::string &name, unsigned int grade);
+	Bureaucrat(const Bureaucrat &b);
+	Bureaucrat &operator=(const Bureaucrat &b);
+	~Bureaucrat() {};
 
-		const std::string &getName() const;
-		unsigned int getGrade() const;
+	const std::string &getName() const;
+	unsigned int getGrade() const;
 };
 
 class GradeTooHighException : public std::exception
 {
-	public:
-		virtual const char *what() const throw();
+public:
+	virtual const char *what() const throw();
 };
 
 class GradeTooLowException : public std::exception
 {
-	public:
-		virtual const char *what() const throw();
+public:
+	virtual const char *what() const throw();
 };
 
 std::ostream &operator<<(std::ostream &out, const Bureaucrat &b);
